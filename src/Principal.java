00bb97fdd111f7fa947e88cab0ac522016f5584a
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2023-02-24 17:24:29 -0600

    Add functionality of creating characters to the game

--- src/Principal.java
+++ src/Principal.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
-/**
- *
- * @author ularg
- */
+import java.util.*;
+import javax.swing.*;
+
 public class Principal extends javax.swing.JFrame {
 
   /**
@@ -25,21 +20,234 @@ public class Principal extends javax.swing.JFrame {
   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
   private void initComponents() {
 
+    jTabbedPane1 = new javax.swing.JTabbedPane();
+    jPanel4 = new javax.swing.JPanel();
+    jLabel1 = new javax.swing.JLabel();
+    jLabel2 = new javax.swing.JLabel();
+    jPanel1 = new javax.swing.JPanel();
+    jLabel3 = new javax.swing.JLabel();
+    jLabel4 = new javax.swing.JLabel();
+    jLabel5 = new javax.swing.JLabel();
+    jLabel6 = new javax.swing.JLabel();
+    jLabel7 = new javax.swing.JLabel();
+    jLabel8 = new javax.swing.JLabel();
+    jLabel9 = new javax.swing.JLabel();
+    jLabel10 = new javax.swing.JLabel();
+    jLabel11 = new javax.swing.JLabel();
+    jButton1 = new javax.swing.JButton();
+    jt_nombrePersonaje = new javax.swing.JTextField();
+    jT_poderPersonaje = new javax.swing.JTextField();
+    jT_debilidadPersonaje = new javax.swing.JTextField();
+    jT_universoPersonaje = new javax.swing.JTextField();
+    jT_fuerzaPersonaje = new javax.swing.JTextField();
+    jT_agilidadFPersonaje = new javax.swing.JTextField();
+    jT_agilidadMPersonaje = new javax.swing.JTextField();
+    jT_puntosVidaPersonaje = new javax.swing.JTextField();
+    jPanel2 = new javax.swing.JPanel();
+    jPanel3 = new javax.swing.JPanel();
+
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+    jLabel1.setFont(new java.awt.Font("Roboto", 1, 20)); // NOI18N
+    jLabel1.setText("Bienvenido al Prototipo del juego de Marvel VS Capcom");
+
+    jLabel2.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
+    jLabel2.setText("Elaborado por Midway Games, Capcom, Marvel y DV");
+
+    javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+    jPanel4.setLayout(jPanel4Layout);
+    jPanel4Layout.setHorizontalGroup(
+      jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(jPanel4Layout.createSequentialGroup()
+        .addContainerGap()
+        .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+          .addComponent(jLabel1)
+          .addComponent(jLabel2))
+        .addContainerGap(52, Short.MAX_VALUE))
+    );
+    jPanel4Layout.setVerticalGroup(
+      jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(jPanel4Layout.createSequentialGroup()
+        .addGap(22, 22, 22)
+        .addComponent(jLabel1)
+        .addGap(18, 18, 18)
+        .addComponent(jLabel2)
+        .addContainerGap(348, Short.MAX_VALUE))
+    );
+
+    jTabbedPane1.addTab("Bienvenido", jPanel4);
+
+    jLabel3.setFont(new java.awt.Font("Roboto", 1, 20)); // NOI18N
+    jLabel3.setText("Agrega un nuevo personaje!! ");
+
+    jLabel4.setText("Nombre:");
+
+    jLabel5.setText("Poder");
+
+    jLabel6.setText("Debilidad:");
+
+    jLabel7.setText("Universo de Pertenencia");
+
+    jLabel8.setText("Fuerza:");
+
+    jLabel9.setText("Agilidad Física:");
+
+    jLabel10.setText("Agilidad Mental:");
+
+    jLabel11.setText("Puntos de Vida:");
+
+    jButton1.setBackground(new java.awt.Color(0, 204, 0));
+    jButton1.setFont(new java.awt.Font("Roboto", 1, 18)); // NOI18N
+    jButton1.setForeground(new java.awt.Color(255, 255, 255));
+    jButton1.setText("Agregar!");
+    jButton1.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        jButton1ActionPerformed(evt);
+      }
+    });
+
+    javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+    jPanel1.setLayout(jPanel1Layout);
+    jPanel1Layout.setHorizontalGroup(
+      jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(jPanel1Layout.createSequentialGroup()
+        .addGap(147, 147, 147)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+          .addComponent(jLabel11)
+          .addComponent(jLabel10)
+          .addComponent(jLabel9)
+          .addComponent(jLabel8)
+          .addComponent(jLabel7)
+          .addComponent(jLabel6)
+          .addComponent(jLabel5)
+          .addComponent(jLabel4))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+          .addComponent(jt_nombrePersonaje)
+          .addComponent(jT_poderPersonaje)
+          .addComponent(jT_debilidadPersonaje)
+          .addComponent(jT_universoPersonaje)
+          .addComponent(jT_fuerzaPersonaje)
+          .addComponent(jT_agilidadFPersonaje)
+          .addComponent(jT_agilidadMPersonaje, javax.swing.GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+          .addComponent(jT_puntosVidaPersonaje))
+        .addContainerGap())
+      .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+        .addGap(128, 128, 128)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+          .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+          .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 363, Short.MAX_VALUE))
+        .addGap(63, 63, 63))
+    );
+    jPanel1Layout.setVerticalGroup(
+      jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(jPanel1Layout.createSequentialGroup()
+        .addGap(15, 15, 15)
+        .addComponent(jLabel3)
+        .addGap(18, 18, 18)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(jLabel4)
+          .addComponent(jt_nombrePersonaje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(jLabel5)
+          .addComponent(jT_poderPersonaje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(jLabel6)
+          .addComponent(jT_debilidadPersonaje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(jLabel7)
+          .addComponent(jT_universoPersonaje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(jLabel8)
+          .addComponent(jT_fuerzaPersonaje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(jLabel9)
+          .addComponent(jT_agilidadFPersonaje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(jLabel10)
+          .addComponent(jT_agilidadMPersonaje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(jLabel11)
+          .addComponent(jT_puntosVidaPersonaje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addGap(26, 26, 26)
+        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addContainerGap(40, Short.MAX_VALUE))
+    );
+
+    jTabbedPane1.addTab("Agregar Personajes", jPanel1);
+
+    javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+    jPanel2.setLayout(jPanel2Layout);
+    jPanel2Layout.setHorizontalGroup(
+      jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGap(0, 554, Short.MAX_VALUE)
+    );
+    jPanel2Layout.setVerticalGroup(
+      jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGap(0, 429, Short.MAX_VALUE)
+    );
+
+    jTabbedPane1.addTab("Listado de Personajes", jPanel2);
+
+    javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+    jPanel3.setLayout(jPanel3Layout);
+    jPanel3Layout.setHorizontalGroup(
+      jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGap(0, 554, Short.MAX_VALUE)
+    );
+    jPanel3Layout.setVerticalGroup(
+      jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGap(0, 429, Short.MAX_VALUE)
+    );
+
+    jTabbedPane1.addTab("Simulación de Batalla", jPanel3);
 
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
     layout.setHorizontalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 400, Short.MAX_VALUE)
+      .addGroup(layout.createSequentialGroup()
+        .addContainerGap()
+        .addComponent(jTabbedPane1)
+        .addContainerGap())
     );
     layout.setVerticalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 300, Short.MAX_VALUE)
+      .addGroup(layout.createSequentialGroup()
+        .addContainerGap()
+        .addComponent(jTabbedPane1)
+        .addContainerGap())
     );
 
     pack();
   }// </editor-fold>//GEN-END:initComponents
+
+  // Metodo de Agregar un personaje con el button
+  private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    // TODO add your handling code here:
+    String nombre = jt_nombrePersonaje.getText();
+    double poder = Double.parseDouble(jT_poderPersonaje.getText());
+    String debilidad = jT_debilidadPersonaje.getText();
+    String universo = jT_universoPersonaje.getText();
+    double fuerza = Double.parseDouble(jT_fuerzaPersonaje.getText());
+    double agilidadFis = Double.parseDouble(jT_agilidadFPersonaje.getText());
+    double agilidadMen = Double.parseDouble(jT_agilidadMPersonaje.getText());
+    double puntosVida = Double.parseDouble(jT_puntosVidaPersonaje.getText());
+    
+    // Crear personaje
+    Personaje nuevoPersonaje = new Personaje(nombre, poder, debilidad, universo, fuerza, agilidadFis, agilidadMen, puntosVida);
+    
+    
+    // Confirmacion de haber agregado personajes
+    JOptionPane.showMessageDialog(this, "¡Personaje agregado con Exito!");
+  }//GEN-LAST:event_jButton1ActionPerformed
 
   /**
    * @param args the command line arguments
@@ -77,5 +285,34 @@ public class Principal extends javax.swing.JFrame {
   }
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
+  private javax.swing.JButton jButton1;
+  private javax.swing.JLabel jLabel1;
+  private javax.swing.JLabel jLabel10;
+  private javax.swing.JLabel jLabel11;
+  private javax.swing.JLabel jLabel2;
+  private javax.swing.JLabel jLabel3;
+  private javax.swing.JLabel jLabel4;
+  private javax.swing.JLabel jLabel5;
+  private javax.swing.JLabel jLabel6;
+  private javax.swing.JLabel jLabel7;
+  private javax.swing.JLabel jLabel8;
+  private javax.swing.JLabel jLabel9;
+  private javax.swing.JPanel jPanel1;
+  private javax.swing.JPanel jPanel2;
+  private javax.swing.JPanel jPanel3;
+  private javax.swing.JPanel jPanel4;
+  private javax.swing.JTextField jT_agilidadFPersonaje;
+  private javax.swing.JTextField jT_agilidadMPersonaje;
+  private javax.swing.JTextField jT_debilidadPersonaje;
+  private javax.swing.JTextField jT_fuerzaPersonaje;
+  private javax.swing.JTextField jT_poderPersonaje;
+  private javax.swing.JTextField jT_puntosVidaPersonaje;
+  private javax.swing.JTextField jT_universoPersonaje;
+  private javax.swing.JTabbedPane jTabbedPane1;
+  private javax.swing.JTextField jt_nombrePersonaje;
   // End of variables declaration//GEN-END:variables
+
+  // Mis Variables
+  private ArrayList<Personaje> personajes = new ArrayList();
+
 }
